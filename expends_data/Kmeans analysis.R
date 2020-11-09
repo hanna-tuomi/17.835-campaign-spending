97ae21efd3cf53fc3cebd1b34408306611051315
hanna-tuomi <hanna.tuomi@gmail.com>
2020-11-08 23:25:11 -0500

    Kmeans analysis

--- expends_data/Kmeans analysis.R
+++ expends_data/Kmeans analysis.R
@@ -18,8 +18,6 @@ plot(clusters, within_ss, main='K clusters vs Within Cluster Sum of Squares')
 
 # get the ideal number of clusters
 mod <- kmeans(X, 7, nstart=50)
-cs <- mod$cluster
-X$cluster <- cs
 
 # get the PCA for graphing 
 scaled_X <- scale(X)
@@ -42,9 +40,15 @@ v2 <- matrix(SVD_X$v[,2], ncol=1)
 X_second_score <- demean_X%*%v2
 X_second_score <- c(X_second_score[,1])
 
+# add clusters + scores to X
+cs <- mod$cluster
+X$cluster <- cs
 X <- cbind(X, X_first_score, X_second_score)
 
-plot(X_first_score, X_second_score, col='white')
+# plot the clusters
+plot(X_first_score, X_second_score, col='white',
+     main='Campaign Spending Strategy Clustering',
+     xlab='PCA First Score', ylab='PCA Second Score')
 cluster_1 <- X[X$cluster==1,]
 cluster_2 <- X[X$cluster==2,]
 cluster_3 <- X[X$cluster==3,]
@@ -53,22 +57,20 @@ cluster_5 <- X[X$cluster==5,]
 cluster_6 <- X[X$cluster==6,]
 cluster_7 <- X[X$cluster==7,]
 
-
-
 text(cluster_1$X_first_score, cluster_1$X_second_score, 
      labels=rep(1,length(cluster_1)), col='blue', cex=0.7)
 text(cluster_2$X_first_score, cluster_2$X_second_score, 
-     labels=rep(2,length(cluster_2)), col='red', cex=0.7)
+     labels=rep(2,length(cluster_2)), col='brown1', cex=0.7)
 text(cluster_3$X_first_score, cluster_3$X_second_score, 
-     labels=rep(3,length(cluster_3)), col='purple', cex=0.7)
+     labels=rep(3,length(cluster_3)), col='darkorchid2', cex=0.7)
 text(cluster_4$X_first_score, cluster_4$X_second_score, 
-     labels=rep(4,length(cluster_4)), col='yellow', cex=0.7)
+     labels=rep(4,length(cluster_4)), col='gold2', cex=0.7)
 text(cluster_5$X_first_score, cluster_5$X_second_score, 
      labels=rep(5,length(cluster_5)), col='gray', cex=0.7)
 text(cluster_6$X_first_score, cluster_6$X_second_score, 
-     labels=rep(6,length(cluster_6)), col='green', cex=0.7)
+     labels=rep(6,length(cluster_6)), col='forestgreen', cex=0.7)
 text(cluster_7$X_first_score, cluster_7$X_second_score, 
-     labels=rep(7,length(cluster_7)), col='orange', cex=0.7)
+     labels=rep(7,length(cluster_7)), col='darkorange1', cex=0.7)
 
 
 
