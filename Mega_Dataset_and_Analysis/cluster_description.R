c77dc128c03da7c4fbc6e506d7decbde7fdd1b63
hanna-tuomi <hanna.tuomi@gmail.com>
2020-12-08 23:14:12 -0500

    Adding error bars for 1 sd

--- Mega_Dataset_and_Analysis/cluster_description.R
+++ Mega_Dataset_and_Analysis/cluster_description.R
@@ -2,13 +2,33 @@
 library(ggplot2)
 clusters <- read.csv('mega_data_with_clusters.csv')
 
+# error bars 
+library('matrixStats')
 
 overall.spending <- colMeans(clusters[,6:16])
+sd.spending <- colSds(as.matrix(clusters[,6:16]))
+
 overall.perc.incumbent<- nrow(clusters[clusters$incumbent == '(I)',])/nrow(clusters)
+is <- nrow(clusters[clusters$incumbent == '(I)',])
+sd.perc.incumbent <- sqrt(((is-overall.perc.incumbent)^2 + 
+                             ((nrow(clusters)-is)-overall.perc.incumbent)^2)/(nrow(clusters)-1))
+
 overall.perc.D <- nrow(clusters[clusters$PARTY == 'D',])/nrow(clusters)
+Ds <- nrow(clusters[clusters$PARTY == 'D',])
+sd.perc.D <-sqrt(((Ds-overall.perc.D)^2 + 
+                    ((nrow(clusters)-Ds)-overall.perc.D)^2)/(nrow(clusters)-1))
+
 overall.perc.unopposed.primary <- nrow(clusters[clusters$PRIMARY == 'Unopposed',])/nrow(clusters)
+unopposed <- nrow(clusters[clusters$PRIMARY == 'Unopposed',])
+sd.perc.unopposed.primary <- sqrt(((unopposed-overall.perc.unopposed.primary)^2 + 
+                                     ((nrow(clusters)-unopposed)-overall.perc.unopposed.primary)^2)/(nrow(clusters)-1))
+
 overall.perc.woman <- mean(clusters$woman)
+women <- nrow(clusters[clusters$woman == 1,])
+sd.perc.woman <- sqrt(((women-overall.perc.woman)^2 + ((nrow(clusters)-women)-overall.perc.woman)^2)/(nrow(clusters)-1))
+
 overall.demo.means <- colMeans(clusters[,39:63])
+sd.demo.means <- colSds(as.matrix(clusters[,39:63]))
 
 columns <- c('Cluster', colnames(clusters[,6:16]), 'perc.incumbent', 'perc.D', 'perc.prim.unopposed', 
              'perc.woman', colnames(clusters[,39:63]))
@@ -17,6 +37,11 @@ summaries <- c('Overall',overall.spending, overall.perc.incumbent, overall.perc.
 
 summary <- rbind(columns, summaries) 
 colnames(summary) <- columns
+
+sd.vector <- c('Overall', sd.spending, sd.perc.incumbent, sd.perc.D, sd.perc.unopposed.primary,
+               sd.perc.woman, sd.demo.means)
+sds<- rbind(columns, sd.vector)
+sds <- as.data.frame(sds[2,2:41])
 
 for (i in 1:7){
   cluster <- clusters[clusters$cluster ==i,]
@@ -33,6 +58,8 @@ for (i in 1:7){
 
 summary <- as.data.frame(summary[2:9,2:ncol(summary)])
 
+
+
 # plotting the differences
 distance_to_means <- data.frame()
 for(i in 2:8) {
@@ -45,112 +72,22 @@ var <- labels(distance_to_means)
 var <- as.character(var)
 
 # plot the differences to the mean
-par(las=1, mar=c(7,20,10,5))
+par(las=1, mar=c(5,10,5,3))
 plot(as.matrix(distance_to_means[1,]),c(1:40), col='red', yaxt='n',
-     xlab='Difference in Cluster to Overall Mean', ylab='Variable', pch=15)
-axis(2, at=1:40, labels=colnames(distance_to_means), cex.axis=1)
-points(as.matrix(distance_to_means[5,]),c(1:40), col='blue', pch=16)
-points(as.matrix(distance_to_means[6,]), c(1:40), col='purple', pch=17)
+     xlab='Difference in Cluster to Overall Mean', ylab='Variable', pch=15, cex=1,
+     main='Difference in Means for Spending Strategies vs Overall')
+axis(2, at=1:40, labels=colnames(distance_to_means), cex.axis=0.3)
+points(as.matrix(distance_to_means[5,]),c(1:40), col='blue', pch=16,cex=1)
+points(as.matrix(distance_to_means[6,]), c(1:40), col='purple', pch=17,cex=1)
 abline(v=0)
-abline(h=11.5, col='gray')
-abline(h=15.5, col='gray')
+abline(h=11.5, col='black')
+abline(h=15.5, col='black')
 legend(0.25,35, legend=c('Strategy 1', 'Strategy 5', 'Strategy 6'), 
-       col=c('red', 'blue', 'purple'), pch=c(15, 16,17), cex=3)
+       col=c('red', 'blue', 'purple'), pch=c(15, 16,17), cex=0.5)
 
-
-
-
-
-
-
-
-
-# clustering approach
-
-dem.clusters <- clusters[,27:63]
-dem.clusters[dem.clusters$incumbent == '(I)',]$incumbent  <- 1
-dem.clusters[dem.clusters$incumbent == '',]$incumbent <- 0
-dem.clusters[dem.clusters$PARTY == 'D',]$PARTY <- 1
-dem.clusters[dem.clusters$PARTY == 'R',]$PARTY <- 0
-dem.clusters[dem.clusters$PARTY != 1 & dem.clusters$PARTY != 0 ,]$PARTY <- 2
-dem.clusters[dem.clusters$GE.perc == '',]$GE.perc <- 0
-dem.clusters <- subset(dem.clusters, select = -c(2,3,4,6,8,9,10))
-dem.clusters <- subset(dem.clusters, select = -c(3,4))
-
-for (c in 1:ncol(dem.clusters)){
-  dem.clusters[,c] <- as.numeric(gsub(x=dem.clusters[,c], pattern='%', replacement=''))
+for (i in 1:40){
+  s <- as.numeric(sds[i,])
+  arrows(x0=-s, y0=i, x1=s, y1=i, code=3, col="darkgray", lwd=0.3, angle=90, length=0.025)
 }
-
-dem.clusters[is.na(dem.clusters)]<-0
-
-# find ideal number of clusters
-cs = c(2:15)
-within_ss <- c()
-
-for (k in cs){
-  k_mean_test <- kmeans(dem.clusters, k)
-  within_ss <- append(within_ss, k_mean_test$tot.withinss)
-}
-
-plot(cs, within_ss, main='K clusters vs Within Cluster Sum of Squares', 
-     xlab='Number of Clusters', ylab='Within Cluster SS')
-
-mod <- kmeans(dem.clusters, 7, nstart=50)
-
-# get the PCA for graphing 
-scaled_X <- scale(dem.clusters)
-X_PCA <- prcomp(scaled_X)
-screeplot(X_PCA, main='PCA for House Race Dem. 2010-2014', xlab='Component')
-biplot(X_PCA, cex=0.6, main='PCA Biplot for House House Race Dem. 2010-2014' )
-
-demean <- function(xmat){
-  apply(xmat, 2, function(z) z - mean(z))
-}
-
-demean_X <- demean(dem.clusters)
-SVD_X<- svd(demean_X)
-
-# use lecture slides to get the scores per country
-v1 <- matrix(SVD_X$v[,1], ncol=1) 
-X_first_score <- demean_X%*%v1
-X_first_score <- c(X_first_score[,1])
-v2 <- matrix(SVD_X$v[,2], ncol=1) 
-X_second_score <- demean_X%*%v2
-X_second_score <- c(X_second_score[,1])
-
-# add clusters + scores to X
-CS <- mod$cluster
-dem.clusters$cluster <- CS
-dem.clusters<- cbind(dem.clusters, X_first_score, X_second_score)
-
-# plot the clusters
-plot(X_first_score, X_second_score, col='white',
-     main='House Campaign Dem. Clustering 2010-2014',
-     xlab='PCA First Score', ylab='PCA Second Score')
-cluster_1 <- dem.clusters[clusters$cluster==1,]
-cluster_2 <- dem.clusters[clusters$cluster==2,]
-cluster_3 <- dem.clusters[clusters$cluster==3,]
-cluster_4 <- dem.clusters[clusters$cluster==4,]
-cluster_5 <- dem.clusters[clusters$cluster==5,]
-cluster_6 <- dem.clusters[clusters$cluster==6,]
-cluster_7 <- dem.clusters[clusters$cluster==7,]
-
-
-text(cluster_1$X_first_score, cluster_1$X_second_score, 
-     labels=rep(1,length(cluster_1)), col="#66C2A5", cex=1)
-text(cluster_2$X_first_score, cluster_2$X_second_score, 
-     labels=rep(2,length(cluster_2)), col="#FC8D62", cex=1)
-text(cluster_3$X_first_score, cluster_3$X_second_score, 
-     labels=rep(3,length(cluster_3)), col="#8DA0CB", cex=1)
-text(cluster_4$X_first_score, cluster_4$X_second_score, 
-     labels=rep(4,length(cluster_4)), col="#E78AC3", cex=1)
-text(cluster_5$X_first_score, cluster_5$X_second_score, 
-     labels=rep(5,length(cluster_5)), col="#A6D854", cex=1)
-text(cluster_6$X_first_score, cluster_6$X_second_score, 
-     labels=rep(6,length(cluster_6)), col="#FFD92F", cex=1)
-text(cluster_7$X_first_score, cluster_7$X_second_score, 
-     labels=rep(7,length(cluster_7)), col="#E5C494", cex=1)
-
-
 
 
