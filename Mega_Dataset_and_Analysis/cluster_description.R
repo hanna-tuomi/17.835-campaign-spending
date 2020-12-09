dae1091237128739cefa2ed56a7bd01d88bd9542
hanna-tuomi <hanna.tuomi@gmail.com>
2020-12-08 22:36:37 -0500

    edits to graph

--- Mega_Dataset_and_Analysis/cluster_description.R
+++ Mega_Dataset_and_Analysis/cluster_description.R
@@ -46,13 +46,16 @@ var <- as.character(var)
 
 # plot the differences to the mean
 par(las=1, mar=c(7,20,10,5))
-plot(as.matrix(distance_to_means[1,]),c(1:40), col='red', yaxt='n')
+plot(as.matrix(distance_to_means[1,]),c(1:40), col='red', yaxt='n',
+     xlab='Difference in Cluster to Overall Mean', ylab='Variable', pch=15)
 axis(2, at=1:40, labels=colnames(distance_to_means), cex.axis=1)
-points(as.matrix(distance_to_means[5,]),c(1:40), col='blue')
-points(as.matrix(distance_to_means[6,]), c(1:40), col='purple')
+points(as.matrix(distance_to_means[5,]),c(1:40), col='blue', pch=16)
+points(as.matrix(distance_to_means[6,]), c(1:40), col='purple', pch=17)
 abline(v=0)
 abline(h=11.5, col='gray')
 abline(h=15.5, col='gray')
+legend(0.25,35, legend=c('Strategy 1', 'Strategy 5', 'Strategy 6'), 
+       col=c('red', 'blue', 'purple'), pch=c(15, 16,17), cex=3)
 
 
 
